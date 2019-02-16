24b2c2a8b0a6396772bf17c6a05647b4d51c29c4
James Cox <jamesncox@yahoo.com>
2019-02-16 14:07:07 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,7 +4,8 @@ def loop_iterator(number_of_times)
   loop do 
     counter += 1
     puts "Welcome to Flatiron School's Web Development Course!"
-      break
+      when counter >= 7
+        break
     end
     
 end
